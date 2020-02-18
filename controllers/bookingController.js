f21bfb06eb78acfae13c7c25846aee102a64da2a
Ricardo Sandez <ricardosandez67@gmail.com>
2020-02-18 00:13:52 -0500

    Fixed typo in webhookCheckout()

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -31,7 +31,9 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
       {
         name: `${tour.name} Tour`,
         description: tour.summary,
-        images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+        images: [
+          `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`
+        ],
         amount: tour.price * 100, // Multiply the price by 100 to convert it to cents
         currency: 'usd',
         quantity: 1
@@ -70,7 +72,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async session => {
   const tour = session.client_reference_id; // We defined this as req.params.tourId in getCheckoutSession() up there.
   const user = (await User.findOne({ email: session.customer_email })).id; // Query for the user using its email and extract its id
-  const price = session.line_items[0].amount / 100;
+  const price = session.display_items[0].amount / 100;
 
   await Booking.create({ tour, user, price });
 };
@@ -91,7 +93,7 @@ exports.webhookCheckout = (req, res, next) => {
   }
 
   // Create a booking with the 'session' (it is at checkout.session.complete)
-  if (event.type === 'checkout.session.complete')
+  if (event.type === 'checkout.session.completed')
     createBookingCheckout(event.data.object);
 
   res.status(200).json({ received: true });
